1655055e613a57a17332acfaa56db5beed3c6ebb
fedeloxe <loxeee@gmail.com>
2022-12-13 16:42:04 +0100

    milestone 0 done

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let team = [
+    {
+        nome: "Wayne Barnett",
+        cognome: "Barnett",
+        role: "Founder & CEO",
+        image:"wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        nome: "Angela",
+        cognome: "Caroll",
+        role: "Chief Editor",
+        image:"angela-caroll-chief-editor.jpg",
+
+    },
+    {
+        nome: "Walter",
+        cognome: "Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg",
+    },
+
+    {    
+        nome:"Angela" ,
+        cognome:"Lopez" ,
+        role: "Social Media Manager",
+        image:"angela-lopez-social-media-manager.jpg",
+
+    },
+    
+    {    
+        nome:"Scott " ,
+        cognome:"Estrada" ,
+        role: "Developer",
+        image:"scott-estrada-developer.jpg",
+
+    },
+    
+    {    
+        nome:"Barbara" ,
+        cognome:"Ramos" ,
+        role: "Graphic Designer",
+        image:"barbara-ramos-graphic-designer.jpg",
+
+    }
+]
+
